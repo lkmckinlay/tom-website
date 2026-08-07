a4a84e275bf30b1b48952882cbdec64eb73c68eb
lkmckinlay <lkmckinlay@waimea.school.nz>
2026-08-07 13:01:12 +1200

    asd

--- Jean_Paul_Clicker/script.js
+++ Jean_Paul_Clicker/script.js
@@ -66,7 +66,7 @@ function MoreFish () {
         if (amountOfFish == 1) {
             image = "./assests/jp_with_fish.jpeg"
             const elements = document.querySelectorAll('[id="pic"]');
-            elements.forEach(elements => {
+            elements.forEach(pic => {
                 pic.src = image;
               });
         
